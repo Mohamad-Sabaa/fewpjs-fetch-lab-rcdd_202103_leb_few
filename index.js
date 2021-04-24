41ab99486f696dfee4ffa805110edfc8d31d12e5
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-24 14:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   fetch('https://anapioficeandfire.com/api/books')
   .then(function(response) {
-    console.log(response);
    return response.json();
- }).then(function(json) {
+ })
+ .then(function(json) {
    renderBooks(json);
- console.log(JSON);
 });
-
  }
 function renderBooks(books) {
   const main = document.querySelector('main');
